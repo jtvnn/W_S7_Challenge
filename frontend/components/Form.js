5066a17450192e0d101440f20b8ebf75305ef7de
Jason Vann <vannjt1@icloud.com>
2025-07-05 20:02:53 -0400

    wiring up the form

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -10,7 +10,15 @@ const validationErrors = {
 
 // 👇 Here you will create your schema.
 const schema = Yup.object().shape({
-
+  fullName: Yup.string()
+    .min(3, validationErrors.fullNameTooShort)
+    .max(20, validationErrors.fullNameTooLong)
+    .required('full name is required'),
+  size: Yup.string()
+    .oneOf(['S', 'M', 'L'], validationErrors.sizeIncorrect)
+    .required('size is required'),
+  toppings: Yup.array().of(Yup.string())
+  
 })
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
@@ -22,46 +30,132 @@ const toppings = [
 ]
 
 export default function Form() {
+  // going to set state for form and errors
+  const [values, setValues] = useState({
+    fullName: '',
+    size: '',
+    toppings: [],
+  });
+
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [submitError, setSubmitError] = useState(false);
+
+  // going to validate on all changes
+  const validate = async (fieldValues = values) => {
+    try {
+      await schema.validate(fieldValues, { abortEarly: false });
+      setErrors({});
+      return true;
+    } catch (err) {
+    const errObj = {};
+    err.inner.forEach(e => {
+      errObj[e.path] = e.message
+    })
+    setErrors(errObj);
+    return false;
+  }};
+
+  // going to handle input changes
+  const handleChange = e => {
+    const { name, value, type, check } = e.target
+    if (type === 'checkbox') {
+      setValues(prev => {
+        const toppingsArr = prev.toppings.includes(value)
+        ? prev.toppings.filter(t => t !== value)
+        : [...prev.toppings, value]
+      })
+    } else {
+      setValues(prev => {
+        const newVals = { ...prev, [name]: value };
+        validate(newVals);
+        return newVals;
+      });
+    }
+  }
+
+  // going to handle touched blur
+  const handleBlur = e => {
+    setTouched(prev => ({ ...prev, [e.target.name]: true}));
+  }
+
+  // going to handle submission
+  const handleSubmit = async e => {
+    e.preventDefault() // prevent reloading
+    const isValid = await validate();
+    setTouched({ fullName: true, size: true }); // mark them all as touched
+    if (isValid) {
+      setSubmitSuccess(true);
+      setSubmitError(false);
+    }
+  }
+
+  const isFormValid = Object.keys(errors).length === 0 && values.fullName && values.size;
+
   return (
     <form>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {submitSuccess && <div className='success'>Thank you for your order!</div>}
+      {submitError && <div className='failure'>Something went wrong</div>}
 
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input placeholder="Type full name" 
+                 id="fullName" 
+                 type="text"
+                 value={values.fullName}
+                 onChange={handleChange}
+                 onBlur={handleBlur} />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {touched.fullName && errors.fullName && <div className='error'>errors.fullName</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size"
+                  name='size'
+                  value={values.size}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+            >
             <option value="">----Choose Size----</option>
             {/* Fill out the missing options */}
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {touched.size && errors.size  && <div className='error'>{errors.size}</div>}
       </div>
 
       <div className="input-group">
         {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
+        {/*<label key="1">
           <input
             name="Pepperoni"
             type="checkbox"
           />
           Pepperoni<br />
-        </label>
+        </label> */}
+        {toppings.map((topping) => (
+          <label key={topping.topping_id}>
+            <input
+              type='checkbox'
+              name='toppings'
+              value={topping.text}
+              checked={values.toppings.includes(topping.text)}
+              onChange={handleChange}
+            />
+            {topping.text}
+            <br />
+          </label>
+        ))}
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
+      <input type="submit" disabled={isFormValid} />
     </form>
   )
 }
