e6fbb18897c2d93296c5f0c5b40d74983fcc8622
Jason Vann <vannjt1@icloud.com>
2025-07-08 21:44:50 -0400

    Unit test and Integration test

--- mvpB.test.js
+++ mvpB.test.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-describe('Sprint 7 Challenge Learner Tests', () => {
+describe("Sprint 7 Challenge Learner Tests", () => {
+  describe("sum function", () => {
+    test("throws error when called with no arguments", () => {
+      expect(() => sum()).toThrow("pass valid numbers");
+    });
+
+    test("throws error when called with invalid string argument", () => {
+      expect(() => sum(2, "seven")).toThrow("pass valid numbers");
+    });
+
+    test("returns 4 when called with (1, 3)", () => {
+      expect(sum(1, 3)).toBe(4);
+    });
+
+    test("returns 3 when called with ('1', 2)", () => {
+      expect(sum("1", 2)).toBe(3);
+    });
+
+    test("returns 13 when called with ('10', '3')", () => {
+      expect(sum("10", "3")).toBe(13);
+    });
+  });
   /*
   👉 TASK 1 - Unit Testing of sum function at the bottom of this module
 
@@ -14,7 +35,41 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [4] sum('1', 2) // returns 3
     [5] sum('10', '3') // returns 13
   */
+  describe("HelloWorld Integration Tests", () => {
+    beforeEach(() => {
+      render(<HelloWorld />);
+    });
 
+    it("renders a link that reads 'Home'", () => {
+      const homeLink = screen.queryByText("Home");
+      expect(homeLink).toBeInTheDocument();
+    });
+
+    it("renders a link that reads 'About'", () => {
+      const aboutLink = screen.queryByText("About");
+      expect(aboutLink).toBeInTheDocument();
+    });
+
+    it("renders a link that reads 'Blog'", () => {
+      const blogLink = screen.queryByText("Blog");
+      expect(blogLink).toBeInTheDocument();
+    });
+
+    it("renders a link that reads 'The Truth'", () => {
+      const truthText = screen.queryByText("The Truth");
+      expect(truthText).toBeInTheDocument();
+    });
+
+    it("renders a link that reads 'JavaScript is pretty awesome'", () => {
+      const jsText = screen.queryByText("JavaScript is pretty awesome");
+      expect(jsText).toBeInTheDocument();
+    });
+
+    it("renders a text that includes 'javaScript is pretty' (case-insensitive, partial)", () => {
+      const partialText = screen.queryByText(/javascript is pretty/i, {exact: false});
+      expect(partialText).toBeInTheDocument();
+    });
+  });
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,18 +84,18 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  //test('you can comment out this test', () => {
-  //  expect(true).toBe(false)
-  })
-})
+  /*test('you can comment out this test', () => {
+    expect(true).toBe(false)
+  })*/
+});
 
 function sum(a, b) {
-  a = Number(a)
-  b = Number(b)
+  a = Number(a);
+  b = Number(b);
   if (isNaN(a) || isNaN(b)) {
-    throw new Error('pass valid numbers')
+    throw new Error("pass valid numbers");
   }
-  return a + b
+  return a + b;
 }
 
 function HelloWorld() {
@@ -48,9 +103,9 @@ function HelloWorld() {
     <div>
       <h1>Hello World Component</h1>
       <nav>
-        <a href='#'>Home</a>
-        <a href='#'>About</a>
-        <a href='#'>Blog</a>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
       </nav>
       <main>
         <section>
@@ -59,5 +114,5 @@ function HelloWorld() {
         </section>
       </main>
     </div>
-  )
+  );
 }
